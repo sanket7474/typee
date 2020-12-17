427785425e5aff56dab79d0b8cab8d108aab08b1
sanket7474 <sanketmaske10@gmail.com>
2020-12-17 15:44:12 +0530

    btn function added

--- assets/js/setup.js
+++ assets/js/setup.js
@@ -1,44 +1,76 @@
 
 const head = document.getElementById('head')
 const subHead = document.getElementById('subHead')
-let i = 0;
-let arr = ["verbicide","misapplying","postbellum","tenanting","noose","airings","egyptians","insoul","oscinine","tali","premises","candidatures","undershoot","genitor","virls","beefsteak","piggery","paleoecologic","versts","vervains","nonallergic","segregations","kayakers","misstep","hypopneas","variers","freehand","expresser","partings","pottering","corpsman","jacking","somedeal","chantey","stationary","neeps","reshingles","disputable","peccancies","hagbut","unbathed","depict","uttered","glees","subscripts","charabancs","snakier","monarchy","intricate","foodies"]
+const data = document.getElementById('data')
+const btn = document.getElementById('btn')
 
+let i = 0;
+let counter = 0;
+let arr = ["2","1","Go!","noose","airings","egyptians","insoul","oscinine","tali","premises","candidatures","undershoot","genitor","virls","beefsteak","piggery","paleoecologic","versts","vervains","nonallergic","segregations","kayakers","misstep","hypopneas","variers","freehand","expresser","partings","pottering","corpsman","jacking","somedeal","chantey","stationary","neeps","reshingles","disputable","peccancies","hagbut","unbathed","depict","uttered","glees","subscripts","charabancs","snakier","monarchy","intricate","foodies"]
+let int = undefined 
 
 const start = function() {
+
+    data.style.animationName = 'fadeOut';
+    data.style.animationDuration = '0.8s';
+    data.style.animationTimingFunction = 'ease-int-out';
+    
+    setTimeout(astart , 800)
 
     
 }
 
+const astart = function() {
+
+    btn.style.display = 'none';
+
+    head.style.animationName = 'fadeIn';
+    head.style.animationDuration = '1s';
+    head.style.animationTimingFunction = 'ease-int-out';
+    head.innerText = 'Get Ready!';
+
+
+    subHead.style.animationName = 'fadeIn';
+    subHead.style.animationDuration = '1s';
+    subHead.style.animationTimingFunction = 'ease-int-out';
+    subHead.innerText = '3';
+    int  = setInterval(anime , 1000)
+
+    }
 
 const anime = function() {
 
-   
-    head.style.animationName = 'fadeOut';
+    counter += 1;
+    if(counter == 5)
+        clearInterval(int)
+    
+        head.style.animationName = 'fadeOut';
     head.style.animationDelay = '0s';
     head.style.animationDuration = '0.5s';
     head.style.animationTimingFunction = 'ease-int-out';
     
-    setTimeout(one , 300);
+    setTimeout(one(subHead.innerText) , 300);
 
 }
-const one = function() {
 
-    head.innerText = subHead.innerText;
+
+const one = function(txt) {
+
+    head.innerText = txt;
 
     head.style.animationName = 'drag';
     head.style.animationDelay = '0s';
     head.style.animationDuration = '0.3s';
     head.style.animationTimingFunction = 'ease-int-out';
 
-    setTimeout(two , 100);
+    setTimeout(two(arr[i++]) , 100);
 }
 
-const two = function() {
+const two = function(txt) {
 
     console.log('yes');
-    subHead.innerText = arr[i];
-    i += 1;
+    subHead.innerText = txt;
+    
     
     subHead.style.animationName = 'fadeIn';
     subHead.style.animationPlayState = 'running'
